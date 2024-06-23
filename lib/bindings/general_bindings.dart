5036517ada4311d0a4e1c45cbb8d0a0c5155f0c0
mehrsa-rmz <r_mehrsa@yahoo.com>
2024-06-23 22:03:02 +0300

    somments in bindings

--- lib/bindings/general_bindings.dart
+++ lib/bindings/general_bindings.dart
@@ -1,8 +1,8 @@
 import 'package:get/get.dart';
-import '../features/personalization/controllers/address_controller.dart';
-import '../features/shop/controllers/checkout_controller.dart';
-import '../features/shop/controllers/product/images_controller.dart';
-import '../features/shop/controllers/product/variation_controller.dart';
+// import '../features/personalization/controllers/address_controller.dart';
+// import '../features/shop/controllers/checkout_controller.dart';
+// import '../features/shop/controllers/product/images_controller.dart';
+// import '../features/shop/controllers/product/variation_controller.dart';
 import '../utils/helpers/network_manager.dart';
 
 class GeneralBindings extends Bindings {
@@ -12,11 +12,11 @@ class GeneralBindings extends Bindings {
     Get.put(NetworkManager());
 
     /// -- Product
-    Get.put(CheckoutController());
-    Get.put(VariationController());
-    Get.put(ImagesController());
+    // Get.put(CheckoutController());
+    // Get.put(VariationController());
+    // Get.put(ImagesController());
 
-    /// -- Other
-    Get.put(AddressController());
+    // /// -- Other
+    // Get.put(AddressController());
   }
 }
